d5f8af840cc86324fdec49894c5b1992df4ea961
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2020-01-01 16:22:13 +0600

    FlutterProject-(01)Interest Calculator [With validation]

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,7 @@ void main() {
       brightness: Brightness.dark,
     ),
     title: "Interest Calculator App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Interest Calculator"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(8),
-        child: GetWidget(),
-      ),
-    ),
+    home: GetWidget()
   ));
 }
 
@@ -32,6 +24,9 @@ class GetWidget extends StatefulWidget {
 }
 
 class _GetWidgetState extends State<GetWidget> {
+  //vl-Step02: define a Globalkey type var
+  var _formKey = GlobalKey<FormState>();
+
   var minPadding = 5.0;
   var currencies = ['Taka', 'Rupees', 'Dollars', 'Pounds'];
 
@@ -58,121 +53,183 @@ class _GetWidgetState extends State<GetWidget> {
     //tu-step02: To style the text accoring to theme define a variable
     TextStyle textStyle = Theme.of(context).textTheme.title;
 
-    return ListView(
-      children: <Widget>[
-        getImageAsset(),
-        Padding(
-            padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
-            child: TextField(
-              //vgtf-Step02: put a controller type variable
-              controller: principalController,
-              style: textStyle,
-              //tu-step03: put the TextStyle_var in style attribute
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Principal",
-                  hintText: "Enter principal e.g. 1200",
-                  labelStyle: textStyle, //style according to theme
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0))),
-            )),
-        Padding(
-            padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
-            child: TextField(
-              controller: roiController,
-              keyboardType: TextInputType.number,
-              style: textStyle, //style according to theme
-              decoration: InputDecoration(
-                  labelText: "Rate Of Interest",
-                  hintText: "In Percent",
-                  labelStyle: textStyle, //style according to theme
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0))),
-            )),
-        Padding(
-            padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
-            child: Row(
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Interest Calculator"),
+      ),
+      body: Form( //To use validation vl-step01: At Scaffold body place Form widget
+          key: _formKey, //vl-Step03: put Globalkey type var in Form key
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child:ListView(
               children: <Widget>[
-                Expanded(
-                    child: TextField(
-                  controller: termController,
-                  keyboardType: TextInputType.number,
-                  style: textStyle, //style according to theme
-                  decoration: InputDecoration(
-                      labelText: "Term",
-                      hintText: "years e.g 2",
-                      labelStyle: textStyle, //style according to theme
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0))),
-                )),
-                Container(
-                  width: minPadding * 5,
-                ),
-                Expanded(
-                    child: DropdownButton(
-                  items: currencies.map((String inputCurrencyItem) {
-                    return DropdownMenuItem<String>(
-                      value: inputCurrencyItem,
-                      child: Text(inputCurrencyItem),
-                    );
-                  }).toList(),
-                  onChanged: (String inValue) {
-                    setState(() {
-                      setCurrency = inValue;
-                    });
-                  },
-                  value: setCurrency,
-                ))
-              ],
-            )),
-        Padding(
-            padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: RaisedButton(
-                    color:
-                        Theme.of(context).accentColor, //Color modify with theme
-                    textColor: Theme.of(context).primaryColorDark,
+                getImageAsset(),
+                Padding(
+                    padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
+
+                    //vl-step04: use TextFormField instead of TextField
+                    child: TextFormField(
+                      //vl-step05:put a function to get your expected validation
+                      validator: (String userInPrincipal){
+                        if(userInPrincipal.isEmpty){
+                          return "You must put principal value";
+                        }
+                        if(double.tryParse(userInPrincipal)==null){
+                          return "You must input number.";
+                        }
+                        return null;
+                      },
+
+                      //vgtf-Step02: put a controller type variable
+                      controller: principalController,
+                      style: textStyle,
+                      //tu-step03: put the TextStyle_var in style attribute
+                      keyboardType: TextInputType.number,
+                      //For decorate the input field
+                      decoration: InputDecoration(
+                        errorStyle: TextStyle(
+                          color: Colors.yellowAccent,
+                          fontSize: 15.0,
+                        ),
+                          labelText: "Principal",
+                          hintText: "Enter principal e.g. 1200",
+                          labelStyle: textStyle, //style according to theme
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
+                    child: TextFormField(
+                      validator: (String userInPrincipal){
+                        if(userInPrincipal.isEmpty){
+                          return "You must put rate of interest";
+                        }
+                        if(double.tryParse(userInPrincipal)==null){
+                          return "You must input number.";
+                        }
+                        return null;
+                      },
+                      controller: roiController,
+                      keyboardType: TextInputType.number,
+                      style: textStyle, //style according to theme
+                      decoration: InputDecoration(
+                          errorStyle: TextStyle(
+                            color: Colors.yellowAccent,
+                            fontSize: 15.0,
+                          ),
+                          labelText: "Rate Of Interest",
+                          hintText: "In Percent",
+                          labelStyle: textStyle, //style according to theme
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: TextFormField(
+                              validator: (String userInPrincipal){
+                                if(userInPrincipal.isEmpty){
+                                  return "You must put term";
+                                }
+                                if(double.tryParse(userInPrincipal)==null){
+                                  return "Input number.";
+                                }
+                                return null;
+                              },
+                              controller: termController,
+                              keyboardType: TextInputType.number,
+                              style: textStyle, //style according to theme
+                              decoration: InputDecoration(
+                                  errorStyle: TextStyle(
+                                    color: Colors.yellowAccent,
+                                    fontSize: 15.0,
+                                  ),
+                                  labelText: "Term",
+                                  hintText: "years e.g 2",
+                                  labelStyle: textStyle, //style according to theme
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5.0))),
+                            )),
+                        Container(
+                          width: minPadding * 5,
+                        ),
+                        Expanded(
+                            child: DropdownButton(
+                              items: currencies.map((String inputCurrencyItem) {
+                                return DropdownMenuItem<String>(
+                                  value: inputCurrencyItem,
+                                  child: Text(inputCurrencyItem),
+                                );
+                              }).toList(),
+                              onChanged: (String inValue) {
+                                setState(() {
+                                  setCurrency = inValue;
+                                });
+                              },
+                              value: setCurrency,
+                            ))
+                      ],
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: RaisedButton(
+                            color:
+                            Theme.of(context).accentColor, //Color modify with theme
+                            textColor: Theme.of(context).primaryColorDark,
+                            child: Text(
+                              "Calculate",
+                              textScaleFactor: 1.5,
+                            ),
+                            onPressed: () {
+                              //Use setState() method to update action in every time
+                              setState(() {
+                                //vl-step06: put logic to execution in validation
+                                if(_formKey.currentState.validate()){
+                                display = _calculateTotalReturns();
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RaisedButton(
+                            color: Theme.of(context)
+                                .primaryColorDark, //Color modify with theme
+                            textColor: Theme.of(context).primaryColorLight,
+                            child: Text(
+                              "Reset",
+                              textScaleFactor: 1.5,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _reset();
+                              });
+                            },
+                          ),
+                        )
+                      ],
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
                     child: Text(
-                      "Calculate",
-                      textScaleFactor: 1.5,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        //Use setState() method to update action in every time
-                        display = _calculateTotalReturns();
-                      });
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: RaisedButton(
-                    color: Theme.of(context)
-                        .primaryColorDark, //Color modify with theme
-                    textColor: Theme.of(context).primaryColorLight,
-                    child: Text(
-                      "Reset",
-                      textScaleFactor: 1.5,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _reset();
-                      });
-                    },
-                  ),
+                      "$display",
+                      textAlign: TextAlign.center,
+                      style: textStyle,
+                    )
                 )
               ],
-            )),
-        Padding(
-            padding: EdgeInsets.only(top: minPadding, bottom: minPadding),
-            child: Text(
-              "$display",
-              textAlign: TextAlign.center,
-              style: textStyle,
-            ))
-      ],
+            ),
+          )
+      ),
     );
+
+
   }
 
   Widget getImageAsset() {
@@ -201,7 +258,7 @@ class _GetWidgetState extends State<GetWidget> {
   }
 
   void _reset() {
-    principalController.text = '';
+    principalController.text = "";
     roiController.text = "";
     termController.text = "";
     setCurrency = currencies[0];
